cd17c48a496dbabeff5e1d62870ad50f06e93383
gintareben <gintare.benyte@gmail.com>
2023-04-23 23:31:21 +0300

    galas

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const tabs = document.getElementsByClassName("tab")
-const h1 = document.querySelector("h1")
-const buttons = document.getElementsByTagName("button")
-
-
-tabs[0].style.display = "block"
+const tabs = document.getElementsByClassName("tab");
+const h1 = document.querySelector("h1");
+const buttons = document.getElementsByTagName("button");
 
 function showTab(event, id) {
-    for(let i = 0; i < tabs.length; i++){
-        tabs[i].style.display = "none"
-        buttons[i].classList.remove("selected")
-    }
-    document.getElementById(id).style.display = "block"
-    event.currentTarget.classList.add("selected")
-    console.log(event.currentTarget)
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("selected");
+    buttons[i].classList.remove("selected");
+  }
+  document.getElementById(id).classList.add("selected");
+  event.currentTarget.classList.add("selected");
+  console.log(event.currentTarget);
 }
