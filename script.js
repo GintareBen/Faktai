2bd550ac86a935b83b561b53fada90c34b56c4b2
gintareben <gintare.benyte@gmail.com>
2023-04-23 23:12:56 +0300

    before ir after

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ const tabs = document.getElementsByClassName("tab")
 const h1 = document.querySelector("h1")
 const buttons = document.getElementsByTagName("button")
 
-// Parodo pirma taba uzkraunant
+
 tabs[0].style.display = "block"
 
 function showTab(event, id) {
-     // Paslepia visus tabus, kai paleidziama funkcija
     for(let i = 0; i < tabs.length; i++){
         tabs[i].style.display = "none"
-        // nuimti stiliu
         buttons[i].classList.remove("selected")
-
-}
-    // rodo tik ta pagal id paleidziant funkcija
+    }
     document.getElementById(id).style.display = "block"
-
-    // Prideda selected klase prie paspausto mygtuko
     event.currentTarget.classList.add("selected")
     console.log(event.currentTarget)
 }
